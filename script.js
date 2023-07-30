a7794831ee94109657106b836e925f3bc0df72e4
Gulnara1714 <118036580+Gulnara1714@users.noreply.github.com>
2023-07-30 20:23:07 +0600

    uplofding
    uploading

---
+++ script.js
@@ -0,0 +1,114 @@
+// alert ('привет Гульнара')
+
+let year = prompt('Афтомобилиндин маркасын танда')
+for (let year = prompt; year= year; year++){
+    
+}
+if(year== 'audi'){
+    alert('300000сом')
+    alert ('дагы танда')
+
+}else if (year=='ford'){
+    alert('400000сом')
+    alert ('дагы танда')
+    
+}else if (year=='hyundai'){
+    alert('500000сом')
+    alert ('дагы танда')
+    
+}else if (year=='Mercedes'){
+    alert('600000сом')
+    alert ('дагы танда')
+    
+}else if (year=='nissan'){
+    alert('700000сом')
+    alert ('дагы танда')
+    
+}else if (year=='scoda'){
+    alert('800000сом')
+    alert ('дагы танда')
+    
+}else if (year=='volkswagen'){
+    alert('200000сом')
+    alert ('дагы танда')
+
+}else if (year=='BMW'){
+    alert('900000сом')
+    alert ('дагы танда')
+
+}else if (year=='honda'){
+    alert('500000сом')
+    alert ('дагы танда')
+    
+}else if (year=='kia'){
+    alert('200000сом')
+    alert ('дагы танда')
+    
+}else if (year=='mazda'){
+    alert('400000сом')
+    alert ('дагы танда')
+    
+}else if (year=='mitsubichi'){
+    alert('600000сом')
+    alert ('дагы танда')
+    
+}else if (year=='renauld'){
+    alert('800000сом')
+    alert ('дагы танда')
+    
+}else if (year=='toyota'){
+    alert('1000000сом')
+    alert ('дагы танда')
+    
+}else if (year=='lexus'){
+    alert('2000000сом')
+    alert ('дагы танда')
+    
+}else if (year=='tesla'){
+    alert('1000000сом')
+    alert ('дагы танда')
+    
+}else if (year=='cadillac'){
+    alert('2000000сом')
+    alert ('дагы танда')
+    
+}else if (year=='москвич'){
+    alert('50000сом')
+    alert ('дагы танда')
+    
+}else if (year=='УАЗ'){
+    alert('80000сом')
+    alert ('дагы танда')
+}else{
+    alert('не правильно')
+}
+
+
+// const courseStatus = 'ready'
+// if (courseStatus=='ready'){
+//     alert('курс башталды')
+// }else if (courseStatus=='pending'){
+//     alert('курс даярдалып жатат')
+// }else{
+//     alert('курс болбойт')
+// }
+
+// while- пока что
+
+// let i=0
+// while (i<3){
+//     alert(i)
+//     i++ 
+// }
+
+// let i=1
+// while (i<=4){
+//     alert(i)
+//     i++
+// }
+// for (башталышы, шарт,шаг)
+
+// for(let x=1; x<10; x++){
+//     alert(x)
+// }
+
